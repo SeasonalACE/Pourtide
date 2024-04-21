7c7b4e1110e9fc80d0ef139f3f39f3228ca80a3f
Pourman <apourman@protonmail.com>
2024-04-21 02:44:32 -0700

    fix: nerf hollow missile weapons

---
+++ Content/sql/weenies/migrations/20200905000018 nerf_chorozite_quarrels.sql
@@ -0,0 +1,7 @@
+use ace_shard;
+
+UPDATE ace_shard.biota biota
+JOIN biota_properties_string name ON biota.id = name.object_Id AND name.type = 1
+JOIN biota_properties_int damage ON biota.Id = damage.object_Id AND damage.type = 44
+SET damage.value = 36
+WHERE name.value = 'Chorizite Quarrel';
