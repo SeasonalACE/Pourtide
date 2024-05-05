4f44c48954df97f9ac38347e607723e9850c2be0
Pourman <apourman@protonmail.com>
2024-05-04 21:02:42 -0700

    chore: remove no longer needed migration scripts

--- Content/sql/weenies/migrations/20200905000000 AddPositionInstance.sql
+++
@@ -1,2 +0,0 @@
-USE ace_shard;
-ALTER TABLE biota_properties_position ADD COLUMN instance TINYINT UNSIGNED AFTER position_Type;
--- Content/sql/weenies/migrations/20200905000001 ExtendPositionInstanceDatatype.sql
+++
@@ -1,2 +0,0 @@
-USE ace_shard;
-ALTER TABLE biota_properties_position MODIFY COLUMN instance int UNSIGNED;
